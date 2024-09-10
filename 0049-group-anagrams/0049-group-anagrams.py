a2987e2e180b4ef302e3f5295db8df473abdf9f8
YITBAREK ALEMU <160623517+jaeckult@users.noreply.github.com>
2024-09-10 23:22:07 +0300

    Time: 102 ms (16.48%), Space: 19.4 MB (95.1%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        strs.sort(key=lambda x: ''.join(sorted(x)))
+        result = []
+        left = 0
+        while left < len(strs):
+            right = left
+            while right < len(strs) and sorted(strs[left]) == sorted(strs[right]):
+                right += 1
+            result.append(strs[left:right])  
+            left = right
+        return result
+
