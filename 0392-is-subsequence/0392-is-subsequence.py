e0ecc256d0afafe327a2fdc6af7131908b7b86c0
YITBAREK ALEMU <160623517+jaeckult@users.noreply.github.com>
2024-10-04 14:24:23 +0300

    Time: 20 ms (19.49%), Space: 11.7 MB (57.25%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        left = 0
+        right = 0
+        while left < len(s) and right < len(t):
+            if s[left] == t[right]:
+                left += 1
+            right += 1
+        return left == len(s)  
+
