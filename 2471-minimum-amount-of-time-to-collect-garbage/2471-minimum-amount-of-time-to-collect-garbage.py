e7e88c420b0fa14229e32c4dba17b00d95ce0793
YITBAREK ALEMU <160623517+jaeckult@users.noreply.github.com>
2024-11-28 15:46:03 +0300

    Time: 78 ms (67.24%), Space: 38.3 MB (39.12%) - LeetHub

--- 2471-minimum-amount-of-time-to-collect-garbage/2471-minimum-amount-of-time-to-collect-garbage.py
+++ 2471-minimum-amount-of-time-to-collect-garbage/2471-minimum-amount-of-time-to-collect-garbage.py
@@ -1,12 +1,10 @@
-from typing import List
-
 class Solution:
     def garbageCollection(self, garbage: List[str], travel: List[int]) -> int:
         total_time = 0
         last_index_m = last_index_p = last_index_g = -1
         n = len(garbage)
         for i in range(n):
-            total_time += len(garbage[i]) 
+            total_time += len(garbage[i])
             
             if 'M' in garbage[i]:
                 last_index_m = i
@@ -14,12 +12,18 @@ class Solution:
                 last_index_p = i
             if 'G' in garbage[i]:
                 last_index_g = i
-        for i in range(last_index_m):
-            total_time += travel[i]
-        for i in range(last_index_p):
-            total_time += travel[i]
-        for i in range(last_index_g):
-            total_time += travel[i]
+        
+        
+        prefix_sum = [0] * (len(travel)+1)
+        prefix_sum[0] = 0
+        for i in range(1, len(travel)+1):
+            prefix_sum[i] = prefix_sum[i-1] + travel[i-1]
+        print(prefix_sum)
+        if last_index_m != -1:
+            total_time += prefix_sum[last_index_m]
+        if last_index_p != -1:
+            total_time += prefix_sum[last_index_p]
+        if last_index_g != -1:
+            total_time += prefix_sum[last_index_g]
 
         return total_time
-
