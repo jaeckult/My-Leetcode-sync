dd90dbe8b022b07294b3a7c1fd5a74953183731d
YITBAREK ALEMU <160623517+jaeckult@users.noreply.github.com>
2025-01-01 04:08:45 +0300

    Time: 0 ms (100%), Space: 17.9 MB (9.48%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.py
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.py
@@ -1,15 +1,21 @@
 class Solution:
     def maxScore(self, s: str) -> int:
-        n = len(s)
-        count_0 = 0
-        count_1 = s.count('1')
+
+        # Total count of '1's in the string
+        total_ones = s.count('1')
         max_score = 0
-        for i in range(n - 1): 
+        left_zeros = 0
+        
+        # Iterate through the string except for the last character
+        for i in range(len(s) - 1):
             if s[i] == '0':
-                count_0 += 1
+                left_zeros += 1
             else:
-                count_1 -= 1
-            score = count_0 + count_1
-            max_score = max(max_score, score)
+                total_ones -= 1
+            
+            # Calculate score for the current split
+            current_score = left_zeros + total_ones
+            max_score = max(max_score, current_score)
         
         return max_score
+# 4:04AM january 1, 2025. Abrehot Library.
